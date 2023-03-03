7ab3c9162c35b4068f587122e1f6753bd5d7482e
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2023-03-03 13:02:32 +0330

    add eureka discovery server

---
+++ discovery-server/src/test/java/ir/mosi/rostam/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mosi.rostam.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
