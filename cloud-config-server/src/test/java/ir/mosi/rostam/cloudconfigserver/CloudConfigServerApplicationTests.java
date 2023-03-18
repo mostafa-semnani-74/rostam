b5d55400cb0bd73bab59b27fdaf29db6e215f177
Mostafa Semnani <mostafa.semnani.74@gmail.com>
2023-03-18 09:57:45 +0330

    add config server

---
+++ cloud-config-server/src/test/java/ir/mosi/rostam/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.mosi.rostam.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
